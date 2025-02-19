84f39341580f6e6a57db413fe86ef3dfe2e365d6
vincent.zhang <vincent.zhang@oneildata.com>
2025-02-19 15:29:37 +0800

    Updates

---
+++ assets/index.lazy-GN5w0huJ.js
@@ -0,0 +1,1 @@
+import{p as o,j as e}from"./index-ZfCGJTDw.js";const r=o("/_layout/")({component:t});function t(){return e.jsx("div",{children:'Hello "home"!'})}export{r as Route};
---
+++ assets/index.lazy-KTBIOC-O.js
@@ -0,0 +1,1 @@
+import{p as o,j as t}from"./index-ZfCGJTDw.js";const n=o("/_layout/dashboard/")({component:e});function e(){return t.jsx("div",{children:"Dashboard Page"})}export{n as Route};
