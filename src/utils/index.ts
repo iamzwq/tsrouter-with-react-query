84582d4b53b4494649f5365f6c7a3c9d1b9fe033
vincent.zhang <vincent.zhang@oneildata.com>
2025-03-21 14:26:37 +0800

    feat:✨add utils function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,7 @@
 export * from './array';
 export * from './date';
+export * from './event';
 export * from './function';
+export * from './is';
 export * from './json2csv';
 export * from './number';
